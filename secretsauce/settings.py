8e640c83c9828856b0b957f731e36aadcb6a499f
eugenechia95 <eugenechia95@gmail.com>
2020-03-09 00:49:43 +0800

    Added djongo for mongodb

--- secretsauce/settings.py
+++ secretsauce/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'secretsauce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'your-db-name',
+        'HOST': 'your-db-host',
     }
 }
 
