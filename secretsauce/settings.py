ad1a11071293c813caeb372d379639d83342b07b
eugenechia95 <eugenechia95@gmail.com>
2020-03-24 18:04:05 +0800

    revert djongo

--- secretsauce/settings.py
+++ secretsauce/settings.py
@@ -76,9 +76,8 @@ WSGI_APPLICATION = 'secretsauce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'your-db-name',
-        'HOST': 'your-db-host',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
