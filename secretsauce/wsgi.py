b774734b1ad31f67e4942e01a55b12b90dd28608
eugenechia95 <eugenechia95@gmail.com>
2020-06-03 01:11:32 +0800

    fix wsgi

--- secretsauce/wsgi.py
+++ secretsauce/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.settings')
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.settings')
 
 application = get_wsgi_application()
