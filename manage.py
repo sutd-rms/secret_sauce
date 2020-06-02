91b46efa9fa6ac2e400a7ce9f3edb875eb43ec7b
eugenechia95 <eugenechia95@gmail.com>
2020-06-02 20:11:52 +0800

    Added in production config

--- manage.py
+++ manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.settings')
+    if os.environ.get('DJANGO_ENV') == 'production':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretsauce.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
