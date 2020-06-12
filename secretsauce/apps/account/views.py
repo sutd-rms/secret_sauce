c69f236e0a11944156fbd0c5b6db936860bc2ba3
eugenechia95 <39354695+eugenechia95@users.noreply.github.com>
2020-06-12 17:50:43 +0800

    Set up email functionality (#15)
    * set up email functionality
    
    * Account creation (#16)
    
    * Initial commit for input parser
    
    * Completed basic data checks - headers and data type (ensure all values are numbers)
    
    * Moved headers to class variable, and fixed bug which skips checking the cell type of the first row of csv file
    
    * Generic serializers, views, urls done for: Project, Constraint, PredictionModels
    
    * Updated accounts and added script to create superuser
    
    * Cleand up some naming errors
    
    * added migrations
    
    Co-authored-by: eugenechia95 <eugenechia95@gmail.com>
    
    * set up email functionality
    
    * Added send_email for invitation creation
    
    Co-authored-by: Gerald Woo <woogerald@yahoo.com.sg>

--- secretsauce/apps/account/views.py
+++ secretsauce/apps/account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -22,6 +23,17 @@ class InvitationCreator(mixins.CreateModelMixin,
     permission_classes = [permissions.IsAdminUser]
     queryset = Invitation.objects.all()
     serializer_class = InvitationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            email = serializer.data.get('email')
+            invitation_id = serializer.data.get('id')
+            message = "Your invitation code is " + invitation_id + "."
+            print(send_mail('Invitation to the RMS Club', message, 'sutdcapstone22@gmail.com', [email], fail_silently=False))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
